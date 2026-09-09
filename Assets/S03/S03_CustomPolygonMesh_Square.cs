bf83c5a1e852c23836ef2e13dd0337e787015589
daraeprincess <yudarae72@gmail.com>
2026-09-09 19:05:36 +0900

    S03 과제 세 번째 시도

--- Assets/S03/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_CustomPolygonMesh_Square.cs
@@ -9,10 +9,10 @@
           Vector3[] vertices = new Vector3[]
           {
               new Vector3(0f, 1f, 0f),
-              new Vector3(0f, 2f, 1f),
-              new Vector3(1f, 1f, 3f),
-              new Vector3(0f, 4f, 1f),
-              new Vector3(5f, 0f, 1f),
+              new Vector3(0.951f, 0.309f, 0f), 
+              new Vector3(0.588f, -0.809f, 0f), 
+              new Vector3(-0.588f, -0.809f, 0f),
+              new Vector3(-0.951f, 0.309f, 0f)
           };
 
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
